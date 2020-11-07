e31d11f209ea02b4dec5ac2def8cc3fcd1f150a8
Guilherme Quirino <guilhermezq@gmail.com>
2020-11-07 11:19:48 +0200

    using UserDetailsService for in memory

--- src/main/java/ee/sda/javaest1blog/configurations/SecurityConfigurations.java
+++ src/main/java/ee/sda/javaest1blog/configurations/SecurityConfigurations.java
@@ -1,21 +1,30 @@
 package ee.sda.javaest1blog.configurations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/posts/show-posts").permitAll()
-        .and().authorizeRequests().antMatchers("/posts/*").denyAll();
+        http.authorizeRequests().antMatchers("/posts/*")
+                .authenticated().and().formLogin().permitAll();
     }
 
+    @Bean
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user1").password("pass1").roles("USER").build();
+        return new InMemoryUserDetailsManager(user);
+
     }
 }
